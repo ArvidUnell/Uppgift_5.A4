7f4a810bb83d38b8aaabe90a53c33cb63cfbedaa
arun06001 <arun06001@E-PW0234KE>
2023-11-23 12:54:37 +0100

    Add project files.

---
+++ Uppgift_5.A4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Uppgift_5_A4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namnArr = { "Jakob", "Tova", "Johanna", "Molly", "Emilio" };
+
+            byte förstIndex = 0;
+
+            for (int i = 1;i < namnArr.Length; i++)
+            {
+                if (namnArr[i].CompareTo(namnArr[förstIndex]) < 0)
+                {
+                    förstIndex = (byte)i;
+                }
+            }
+
+            Console.WriteLine($"Namnet som kommer först i bokstavsordning är {namnArr[förstIndex]}.");
+
+            Console.WriteLine("\nTryck på valfri knapp för att avsluta");
+            Console.ReadKey();
+        }
+    }
+}
